880b30b9a1d2f8efcfe2d1ebdb17b5de71a5c223
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2022-11-11 20:00:11 -0500

    home screen

--- app/src/main/java/com/example/planti/Home.java
+++ app/src/main/java/com/example/planti/Home.java
@@ -9,19 +9,37 @@ import android.widget.Button;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnPerfil;
+    Button calificar1, calificar2, btnPerfil, btnCrearPlanta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        calificar1 = findViewById(R.id.calificar1);
+        calificar2 = findViewById(R.id.calificar2);
         btnPerfil = findViewById(R.id.btnPerfil);
+        btnCrearPlanta = findViewById(R.id.btnCrearPlanta);
+
+        calificar1.setOnClickListener(this);
+        calificar2.setOnClickListener(this);
         btnPerfil.setOnClickListener(this);
+        btnCrearPlanta.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(this, CreatePlant.class);
-        startActivity(intent);
+        if (view == btnCrearPlanta) {
+            Intent intent = new Intent(this, CreatePlant.class);
+            startActivity(intent);
+        }
+        if(view == btnPerfil){
+            Intent intent = new Intent(this, Profile.class);
+            startActivity(intent);
+        }
+        if(view == calificar1 || view == calificar2){
+            Intent intent = new Intent(this, RatePlant.class);
+            startActivity(intent);
+        }
     }
 }
