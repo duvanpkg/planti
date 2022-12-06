9fa0a250f9e3dc39ec2d19a74cc18a52fae8ea93
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2022-12-05 20:38:49 -0500

    Set home images by while loop

--- app/src/main/java/com/example/planti/ui/main/HomeFragment.java
+++ app/src/main/java/com/example/planti/ui/main/HomeFragment.java
@@ -4,18 +4,25 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.example.planti.Bdsqlite;
 import com.example.planti.CreatePlant;
@@ -33,20 +40,14 @@ import java.util.HashMap;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
-
-    ArrayList<HashMap<String, String>> arraylist = new ArrayList<>();
-    Bdsqlite admin;
-    SQLiteDatabase bd;
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
 
         return fragment;
     }
@@ -64,65 +65,82 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    // Relevant code starts here!!!
+
+    Bdsqlite admin;
+    SQLiteDatabase bd;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         admin = new Bdsqlite(getActivity(), "planti", null, 1);
         bd = admin.getWritableDatabase();
-        try {
-            llenarlista();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void llenarlista() throws JSONException {
         String query = "select * from plants";
         Cursor cursor = bd.rawQuery(query, null);
-        JSONArray ja = null;
 
-        if (cursor.moveToFirst()){
-            while(!cursor.isAfterLast()){
-                HashMap<String, String> map = new HashMap<String, String>();
-                @SuppressLint("Range")
-                String id = cursor.getString(cursor.getColumnIndex("id"));
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+
                 @SuppressLint("Range")
                 String name = cursor.getString(cursor.getColumnIndex("name"));
                 @SuppressLint("Range")
                 String plantKind = cursor.getString(cursor.getColumnIndex("plantKind"));
                 @SuppressLint("Range")
-                String bitmap = cursor.getString(cursor.getColumnIndex("imageBitmap"));
+                byte[] image = cursor.getBlob(cursor.getColumnIndex("imageBitmap"));
                 @SuppressLint("Range")
                 String description = cursor.getString(cursor.getColumnIndex("description"));
 
 
-                map.put("id", id);
-                map.put("name", name);
-                map.put("plantKind", plantKind);
-                map.put("bitmap", bitmap);
-                map.put("description", description);
-                System.out.println(map);
-                arraylist.add(map);
+                TextView tvNombre = new TextView(getActivity());
+                tvNombre.setText(name);
+                tvNombre.setTextSize(30);
+                tvNombre.setTextColor(Color.parseColor("#000000"));
+                tvNombre.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                ImageView iv = new ImageView(getActivity());
+                try{
+                    iv.setImageBitmap(BitmapFactory.decodeByteArray(image, 0, image.length));
+                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500, 500);
+                    layoutParams.gravity=Gravity.CENTER;
+                    iv.setLayoutParams(layoutParams);
+
+                }catch (Exception e){
+                    //show nothing
+                }
+
+                TextView tvTipo = new TextView(getActivity());
+                tvTipo.setText(plantKind);
+                tvTipo.setTextSize(20);
+                tvTipo.setTextColor(Color.parseColor("#000000"));
+                tvTipo.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                TextView tvDescripcion = new TextView(getActivity());
+                tvDescripcion.setText(description);
+                tvDescripcion.setTextSize(15);
+                tvDescripcion.setTextColor(Color.parseColor("#000000"));
+                tvDescripcion.setGravity(Gravity.CENTER_HORIZONTAL);
+
+                Button btn = new Button(getActivity());
+                btn.setText("Calificar");
+                btn.setOnClickListener(this);
+
+                LinearLayout layout = getActivity().findViewById(R.id.layout);
+                layout.addView(tvNombre);
+                layout.addView(tvTipo);
+                layout.addView(tvDescripcion);
+                layout.addView(iv);
+                layout.addView(btn);
 
                 cursor.moveToNext();
             }
         }
+
         cursor.close();
 
-        //2 arreglos
-        String[] origen = new String[4];
-        origen[0] = "name";
-        origen[1] = "plantKind";
-        origen[2] = "description";
-        origen[3] = "bitmap";
+    }
 
-        int[] destino = new int[4];
-        destino[0] = R.id.tvNombre;
-        destino[1] = R.id.tvTipo;
-        destino[2] = R.id.tvDescripcion;
-        destino[3] = R.id.ivPlant;
-        SimpleAdapter listadapter = new SimpleAdapter(getActivity(), arraylist, R.layout.item_home, origen, destino);
-        ListView lvVisitas = getView().findViewById(R.id.lvHome);
-        lvVisitas.setAdapter(listadapter);
-
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(),RatePlant.class);
+        startActivity(intent);
     }
 }
