9c09c0a406dc6fe20ebd02c728c249471dfce319
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2022-12-08 18:14:02 -0500

    Delete profile picture feature

--- app/src/main/java/com/example/planti/EditProfile.java
+++ app/src/main/java/com/example/planti/EditProfile.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class EditProfile extends AppCompatActivity implements View.OnClickListener {
     TextView tvNombre, tvEmail, tvDescripcion;
-    Button btnEditarFoto, btnCancelar, btnGuardar;
+    Button btnCancelar, btnGuardar;
 
 
     @Override
@@ -21,21 +21,16 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
         tvNombre = findViewById(R.id.tvNombre);
         tvEmail = findViewById(R.id.tvEmail);
         tvDescripcion = findViewById(R.id.tvNombre);
-        btnEditarFoto = findViewById(R.id.btnEditarFoto);
         btnCancelar = findViewById(R.id.btnCancelar);
         btnGuardar = findViewById(R.id.btnGuardar);
 
-        btnEditarFoto.setOnClickListener(this);
         btnCancelar.setOnClickListener(this);
         btnGuardar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if(view == btnEditarFoto){
-            Intent intent = new Intent(this, LoadPicture.class);
-            startActivity(intent);
-        }else if(view == btnCancelar){
+        if(view == btnCancelar){
             finish();
         }else if(view == btnGuardar){
             // todo: actualizar información editada en la base de datos
